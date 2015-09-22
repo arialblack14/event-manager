018c578c3b6f21ae3abca36646393644084a4e62
arialblack14 <tsirasalexis@yahoo.gr>
2015-09-23 00:20:21 +0300

    Added READMEl.md

--- lib/event_manager.rb
+++ lib/event_manager.rb
@@ -1,18 +1,20 @@
 require "csv"
+require_relative 'sunlight-congress'
+
+Sunlight::Congress.api_key = "c09586eb79f04ed79664cdb09c3f6bb9"
+
+def clean_zipcode(zipcode)
+	zipcode.to_s.rjust(5, "0")[0..4]
+end
+	
 puts "EventManager initialized!"
 
 contents = CSV.open "event_attendees.csv", headers: true, header_converters: :symbol
 contents.each do |row|
 	name = row[:first_name]
-	zipcode = row[:zipcode]
 
-	zipcode ||= "00000"
+	zipcode = clean_zipcode(row[:zipcode])
+	legislators = Sunlight::Congress::Legislators.by_zipcode(zipcode)
 
-	if zipcode.length < 5
-		zipcode = zipcode.rjust 5, "0"
-	elsif zipcode.length > 5
-		zipcode = zipcode[0..4]
-	end
-
-	puts "#{name} #{zipcode}"
+	puts "#{name} #{zipcode} #{legislators}"
 end
